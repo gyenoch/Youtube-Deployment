267c6e3826657aed1178832c41c5f31bdd48772f
Enoch Gyampoh <136348310+gyenoch@users.noreply.github.com>
2025-01-11 16:53:16 +0000

    Delete sonar-tests directory

--- sonar-tests/insecure-email.js
+++
@@ -1,4 +0,0 @@
-// Test file: insecure-email.js
-const email = req.query.email; // Unvalidated user input
-const dynamicLink = `<a href="mailto:${email}">Click Here</a>`; // Vulnerable code
-console.log(dynamicLink);
