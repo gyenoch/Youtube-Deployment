7006d46ab512ab592ceb3b3f5456f20768ad139d
gyenoch <www.gyenoch@gmail.com>
2025-01-11 16:10:19 +0000

    .github/

--- src/App.js
+++ src/App.js
@@ -17,9 +17,4 @@ const App = () => (
   </BrowserRouter>
 );
 
-// Test file: insecure-email.js
-const email = req.query.email; // Unvalidated user input
-const dynamicLink = `<a href="mailto:${email}">Click Here</a>`; // Vulnerable code
-console.log(dynamicLink);
-
 export default App;
